4894ef5c047909f19386845291f2d83cf021f481
Renzo Quispe Valdez <renzo.quispe.va@tecsup.edu.pe>
2025-05-13 19:19:01 -0500

    Configuracion del config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,10 +1,10 @@
-export default {
-  HOST: "dpg-d0hroceuk2gs73891lsg-a",       // Host interno para conexión desde Render
-  USER: "root",                             // Tu usuario
-  PASSWORD: "ZYLftvSaTWj3xIOeDkmx1fAtxR5E9Cz0", // Tu contraseña segura
-  DB: "auth_db_k52s",                       // Nombre exacto de la base de datos
-  PORT: 5432,                               // Puerto estándar para PostgreSQL
-  dialect: "postgres",                      // Dialecto para Sequelize
+module.exports = {
+  HOST: "dpg-d0hroceuk2gs73891lsg-a",       
+  USER: "root",                             
+  PASSWORD: "ZYLftvSaTWj3xIOeDkmx1fAtxR5E9Cz0", 
+  DB: "auth_db_k52s",                       
+  PORT: 5432,                               
+  dialect: "postgres",                      
   pool: {
     max: 5,
     min: 0,
